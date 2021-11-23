1611df77486d137b1a864529112006b29965788b
jhol2015 <galilleu2011@gmail.com>
2021-11-22 22:38:37 -0300

    Modify fontFamily. :construction:

---
+++ AppShopping/AppShopping/.vshistory/AssemblyInfo.cs/2021-11-22_22_30_47_287.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Light, Alias = MontserratLight")]
+[assembly: ExportFont("Montserrat-Regular, Alias = MontserratRegular")]
+[assembly: ExportFont("Montserrat-SemiBold, Alias = MontserratSemiBold")]
+[assembly: ExportFont("Montserrat-Bold, Alias = MontserratBold")]

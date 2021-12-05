80eb67385d4fc7c15ff4cc69b93ac2bdefea9691
jhol2015 <galilleu2011@gmail.com>
2021-12-04 23:59:36 -0300

    Create page EstablishmentDetail and viewmodel. :construction:

---
+++ AppShopping/AppShopping/ViewModels/EstablishmentDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AppShopping.Libraries.Helpers.MVVM;
+using AppShopping.Models;
+using AppShopping.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppShopping.ViewModels
+{
+    public class EstablishmentDetailViewModel : BaseViewModel
+    {
+        public Establishment Establishment { get; set; }
+        public EstablishmentDetailViewModel()
+        {
+            Establishment = new EstablishmentService().GetEstablishments().First() ;
+        }
+    }
+}
